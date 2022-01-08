25027e01ef047337c19013991ed7fae197ec93e3
Danyelss <mos.dani@yahoo.com>
2022-01-08 20:04:39 +0200

    Register button

--- lib/main.dart
+++ lib/main.dart
@@ -41,60 +41,81 @@ class _MyHomePageState extends State<MyHomePage> {
             fit: BoxFit.cover,
           ),
         ),
-        child: Center(
-          child: Container(
-            width: MediaQuery.of(context).size.width * (2 / 3),
-            height: MediaQuery.of(context).size.height,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                const Text(
-                  'Username',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
+        child: Column(
+          children: [
+            const SizedBox(height: 30),
+            OutlinedButton(
+              style: OutlinedButton.styleFrom(
+                side: const BorderSide(color: Colors.white),
+                minimumSize: const Size(150, 50),
+              ),
+              onPressed: () {
+                // Respond to button press
+              },
+              child: Text(
+                "Register",
+                style: TextStyle(
+                  fontSize: 25,
+                  color: Colors.white.withOpacity(1),
                 ),
-                const SizedBox(height: 10),
-                const TextField(
-                  obscureText: false,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const Text(
-                  'Password',
-                  style: TextStyle(
-                    fontSize: 25,
-                  ),
-                ),
-                const SizedBox(height: 10),
-                const TextField(
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                const SizedBox(height: 10),
-                OutlinedButton(
-                  style: OutlinedButton.styleFrom(
-                    side: const BorderSide(color: Colors.white),
-                    minimumSize: const Size(150, 50),
-                  ),
-                  onPressed: () {
-                    // Respond to button press
-                  },
-                  child: Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 25,
-                      color: Colors.white.withOpacity(1),
-                    ),
-                  ),
-                )
-              ],
+              ),
             ),
-          ),
+            Center(
+              child: Container(
+                width: MediaQuery.of(context).size.width * (2 / 3),
+                height: MediaQuery.of(context).size.height - 30 - 50,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'Username',
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const TextField(
+                      obscureText: false,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const Text(
+                      'Password',
+                      style: TextStyle(
+                        fontSize: 25,
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    const TextField(
+                      obscureText: true,
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 10),
+                    OutlinedButton(
+                      style: OutlinedButton.styleFrom(
+                        side: const BorderSide(color: Colors.white),
+                        minimumSize: const Size(150, 50),
+                      ),
+                      onPressed: () {
+                        // Respond to button press
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 25,
+                          color: Colors.white.withOpacity(1),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
